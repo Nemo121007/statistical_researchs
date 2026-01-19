8a86c927e068992888f003486a9afdc4bd33879b
saveliy <s.gashkov@technodar.group>
2026-01-19 11:26:02 +0300

    Добваление конфига

---
+++ config.py
@@ -0,0 +1,12 @@
+class CorrectorSettings:
+    """
+    Класс параметров алгоритмов обработки географических данных
+    Attributes:
+        Earth_radius_meters: Радиус Земли в метрах
+        defauld_speed_threshold: Значение скорости для фильтрации (в метрах)
+    """
+    # Радиус Земли в метрах
+    Earth_radius_meters: float = 6371000
+
+
+
